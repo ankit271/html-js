d0e885eb1c169dd2e0cfcc4b9d87e1108a97869a
Ankit Mishra <atmishra243@gmail.com>
2025-02-19 08:41:08 +0530

    objects

---
+++ javascript/objects.js
@@ -0,0 +1,43 @@
+const names = [
+    {
+        name: 'Ankit',
+        age: 28,
+        gender: 'Male',
+        rollNo : 101
+    },
+    {
+        name: 'Sumit',
+        age: 25,
+        gender: 'Male',
+        rollNo : 102
+    },
+    {
+        name: 'Ajay',
+        age: 27,
+        gender: 'Male',
+        rollNo : 103
+    }
+]
+
+const a = [1,2,4]
+
+const [one, ...zainab] = a
+
+console.log(one);
+console.log(zainab);
+
+
+
+// const name = names.entries();
+
+// for (const element of name) {
+//     console.log(element);
+// }
+
+
+
+ 
+
+
+// const name = names.filter( name => name.age > 27);
+// console.log(name);
